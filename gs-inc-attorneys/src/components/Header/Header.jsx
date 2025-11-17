9cd0b13ca07bb728bc26314a09d1d6b4b2f4baf6
OEM <omphilestudent@gmail.com>
2025-11-17 16:08:51 +0200

    Initialized the navigation section to better convey the icon and details.

--- gs-inc-attorneys/src/components/Header/Header.jsx
+++ gs-inc-attorneys/src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import './Header.css'
-import logo from "../../assets/logo.png";
+import logo from "../../assets/Logo.png";
 
 
 const Header = () => {
