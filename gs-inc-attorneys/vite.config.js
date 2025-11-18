88558a1262b95659e614042d2402bde98f9bee8c
OEM <omphilestudent@gmail.com>
2025-11-18 09:51:58 +0200

    made new changes

--- gs-inc-attorneys/vite.config.js
+++ gs-inc-attorneys/vite.config.js
@@ -10,6 +10,7 @@ const sharedDir = path.resolve(appRoot, '../shared')
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: './',
     plugins: [react()],
     server: {
         port: 5173,
